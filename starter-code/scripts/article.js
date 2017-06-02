c62ffa38d66fe0cffcc6747bfa5a33647c732002
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-06-02 13:24:56 -0700

    Work done in class--most of the TODOs

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -22,14 +22,16 @@ Article.prototype.toHtml = function() {
   return template(this);
 };
 
-rawData.sort(function(a,b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
+function populateArticles() {
+  rawData.sort(function(a,b) {
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  });
 
-rawData.forEach(function(articleObject) {
-  articles.push(new Article(articleObject));
-})
+  rawData.forEach(function(articleObject) {
+    articles.push(new Article(articleObject));
+  })
 
-articles.forEach(function(article){
-  $('#articles').append(article.toHtml())
-});
+  articles.forEach(function(article){
+    $('#articles').append(article.toHtml())
+  });
+}
